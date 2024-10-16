5a596732235f50331cdd802e61e0f566738e497f
EastonA01 <eastonredwing@gmail.com>
2024-10-16 10:34:06 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This is my mark I leave on the world, thank you Harvard CS50 for starting me on this journey.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
